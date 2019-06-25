36babbf64954e2550718e40378df755e547c041e
Alex Koukoulas <Alex.Koukoulas@king.com>
2019-06-25 18:21:22 +0100

    Added a basic FSM system for the encounter states

--- source/game/encounter/systems/EncounterStateControllerSystem.cpp
+++ source/game/encounter/systems/EncounterStateControllerSystem.cpp
@@ -10,6 +10,8 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 #include "EncounterStateControllerSystem.h"
+#include "../flowstates/BaseEncounterFlowState.h"
+#include "../flowstates/DarkenedOpponentsIntroEncounterFlowState.h"
 #include "../components/EncounterStateSingletonComponent.h"
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -23,13 +25,28 @@ EncounterStateControllerSystem::EncounterStateControllerSystem(ecs::World& world
     //CalculateAndSetComponentUsageMask<AnimatedFlowerTagComponent, RenderableComponent>();
 }
 
-void EncounterStateControllerSystem::VUpdateAssociatedComponents(const float) const
+void EncounterStateControllerSystem::VUpdateAssociatedComponents(const float dt) const
 {
     auto& encounterStateComponent = mWorld.GetSingletonComponent<EncounterStateSingletonComponent>();
-    
-    if (encounterStateComponent.mOverworldEncounterAnimationState == OverworldEncounterAnimationState::ENCOUNTER_INTRO_ANIMATION_COMPLETE)
+        
+    // Battle finished condition
+    if (false)
     {
 
+    }    
+    else if (encounterStateComponent.mActiveEncounterFlowState != nullptr)
+    {
+        encounterStateComponent.mActiveEncounterFlowState->VUpdate(dt);
+
+        if (encounterStateComponent.mActiveEncounterFlowState->mNextFlowState != nullptr)
+        { 
+            encounterStateComponent.mActiveEncounterFlowState = std::move(encounterStateComponent.mActiveEncounterFlowState->mNextFlowState);
+        }
+    }
+    // Battle started condition
+    else if (encounterStateComponent.mOverworldEncounterAnimationState == OverworldEncounterAnimationState::ENCOUNTER_INTRO_ANIMATION_COMPLETE)
+    {
+        encounterStateComponent.mActiveEncounterFlowState = std::make_unique<DarkenedOpponentsIntroEncounterFlowState>(mWorld);
     }
 }
 
